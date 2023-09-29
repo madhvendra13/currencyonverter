4ea5d2b583b5e1bfa0e1f8ada04fdd6eeb4c6850
Madhvendra Singh <chouhanmadhvendra13@gmail.com>
2023-09-30 01:33:41 +0530

    updated

--- cc.py
+++ cc.py
@@ -25,6 +25,22 @@ currency_list=["IND","USD","CNY","DKK","EUR","GBP"]
 
 variable1=StringVar()
 variable2=StringVar()
+txt=StringVar()
+
+
+def convert():
+    from_currency=variable1.get()
+    to_currency=variable2.get()
+    c=CurrencyRates()
+    amt=c.convert(from_currency,to_currency,float(amount_entry.get()))
+    amount=float("{:.3f}".format(amt))
+    txt.set(amount)
+    result_label=customtkinter.CTkLabel(app,textvariable=txt,text_font=('Arial',30,'bold',fg_color="#202630",text_color="#FFFFFF",width=50)
+    result_label.place(x=125,y=350)
+
+
+def reset():
+    amount_entry.delete(0,END)    
 
 
 from_menu=customtkinter.CTkComboBox(app,variable=variable1,values=currency_list,text_font=('Arial',12,'bold'),dropdown_text_font=('Arial',12,'bold'),fg_color="#FFFFFF",text_color="#000000",button_color="#710193",button_hover_color="#710193",border_color="#FFFFFF",dropdown_color="#FFFFFF",dropdown_hover_color="#00FF00",dropdown_text_color="#000000")
@@ -38,10 +54,10 @@ amount_entry=customtkinter.CTkEntry(app,text_font=('Arial',20,'bold'),text_color
 amount_entry.place(x=18,y=240)
 
 
-convert_button=customtkinter.CTkButton(app,text="Convert",text_font=('Arial',20,'bold'),texxt_color="#FFFFFF",fg_color="#710193",hover_color"#710193")
+convert_button=customtkinter.CTkButton(app,command=convert,text="Convert",text_font=('Arial',20,'bold'),texxt_color="#FFFFFF",fg_color="#710193",hover_color"#710193")
 convert_button.place(x=50,y=300)
 
-reset_button=customtkinter.CTkButton(app,text="Reset",text_font=('Arial',20,'bold'),texxt_color="#FFFFFF",fg_color="#bdSb15",hover_color"#bdSb15")
+reset_button=customtkinter.CTkButton(app,command=reset,text="Reset",text_font=('Arial',20,'bold'),texxt_color="#FFFFFF",fg_color="#bdSb15",hover_color"#bdSb15")
 reset_button.place(x=200,y=300)
 
 
